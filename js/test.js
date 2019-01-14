04984bfece24f6c4cdc4c928cbfc6ee36f57a736
naturallzh <naturallzh@163.com>
2019-01-14 11:05:50 +0800

    building

--- js/test.js
+++ js/test.js
@@ -1,13 +1,10 @@
 var vm = new Vue({
 	el: '#main-body',
 	data: {
-		test: "welcome!"
+		test1: "welcome as well!"
 	},
 	created: function () {},
 	computed: {},
 	methods: {
-		goToTest: function () {
-			location.href = 'html/test.html';
-		}
 	}
 });
