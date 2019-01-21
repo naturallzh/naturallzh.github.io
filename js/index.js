2ae2f1895e55c9272c6f4f63adc6250184ffda31
naturallzh <naturallzh@163.com>
2019-01-21 14:56:02 +0800

    building

--- js/index.js
+++ js/index.js
@@ -1,24 +1,14 @@
 let vm = new Vue({
 	el: '#index-body',
 	data: {
-		url: {
-			searchList: "html/search_list.html",
-			newList: "html/new_list.html",
-			comparePage: "html/compare_page.html",
-		},
+		testData: false,
 	},
 	created: function () {},
 	computed: {},
 	methods: {
-		openPage: function (url, tar) {
-			// window.location.href = 'html/search_list.html';
-			window.open(url, tar);
-		},
 		test: function () {
-			$('.btn-search')[0].style.boxShadow = '0 0 80px 0 rgba(0, 0, 0, 0.5)';
-		},
-		test2: function () {
-			$('.btn-search')[0].style.boxShadow = '';
+			this.testData = true;
+			console.log(this.testData);
 		}
 	}
 });
