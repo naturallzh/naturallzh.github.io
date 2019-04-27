01cdbb33c9985a34e86838f8c9ffab3092f3a22f
nuglues <406455645@qq.com>
2019-04-27 22:19:19 +0800

    update

--- js/component.js
+++ js/component.js
@@ -1,10 +1,7 @@
 Vue.component('auto-refresh', {
   template:
-    '<div id="btn-refresh" @click="changeState">' +
-      // '<transition name="progress-bar-decrease">' +
-        // '<div id="progress-bar" v-if="showProgressBar"></div>' +
-          '<div id="progress-bar" :class="heightClass"></div>' +
-      // '</transition>' +
+    '<div id="btn-refresh" @click="shiftSwitch">' +
+      '<div id="progress-bar" :class="heightClass"></div>' +
       '<span class="value">{{delay}}</span>' +
       '<span class="value">{{windowSize[0]}} * {{windowSize[1]}}</span>' +
       '<span class="value">({{mousePos[0]}} , {{mousePos[1]}})</span>' +
@@ -14,7 +11,6 @@ Vue.component('auto-refresh', {
     return {
       refreshTimer: null,             // 刷新页面的计时器
       heightClass: "full-height",     // 高度样式 直接都用.style.cssText的方法修改css也可以
-      currentState: this.state,       // 当前state状态，props中的state无法修改
       windowSize: [],                 // 浏览器可视区域大小
       mousePos: [" - "," - "],        // 鼠标位置
     }
@@ -24,9 +20,9 @@ Vue.component('auto-refresh', {
       type: Number,
       default: 3000
     },
-    state: {
-      type: Boolean,
-      default: true
+    switch: {
+      type: String,
+      default: "on"
     },
     opacity: {
       type: String,
@@ -37,27 +33,33 @@ Vue.component('auto-refresh', {
   computed: {
   },
   watch: {
-    currentState: {
-      handler (newCurrentState, oldCurrentState) {
-        if (newCurrentState) {
-          this.refreshTimer = setTimeout(()=>{window.location.reload()},this.delay);
-          setTimeout(()=>{
-            this.heightClass = "zero-height";
-            setStyle("#progress-bar",[0],"transition-duration:" + this.delay/1000 + "s");
-          },0);
-        }
-        else {
-          clearTimeout(this.refreshTimer);
-          this.heightClass = "full-height";
-          // setStyle("#progress-bar",[0],"transition: none");
-        }
-      },
-      immediate: true
-    }
   },
   methods: {
-    changeState: function () {
-      this.currentState = !this.currentState;
+    // 开启自动刷新时
+    switchOn: function () {
+      this.refreshTimer = setTimeout(()=>{window.location.reload()},this.delay);
+      setTimeout(()=>{
+        this.heightClass = "zero-height";
+        setStyle("#progress-bar",[0],"transition-duration:" + this.delay/1000 + "s");
+      },0);
+    },
+
+    // 关闭自动刷新时
+    switchOff: function () {
+      clearTimeout(this.refreshTimer);
+      this.heightClass = "full-height";
+    },
+
+    // 切换是否刷新
+    shiftSwitch: function () {
+      if (sessionStorage.getItem('currentSwitch') === "on") {
+        this.switchOff();
+        sessionStorage.setItem('currentSwitch','off');
+      }
+      else {
+        this.switchOn();
+        sessionStorage.setItem('currentSwitch','on');
+      }
     },
 
     // 获取浏览器窗口大小
@@ -73,6 +75,17 @@ Vue.component('auto-refresh', {
 
   beforeCreate () {},
   created () {
+    if ((sessionStorage['currentSwitch'] || this.switch) === "on") {
+      // sessionStorage 的另一种取值写法 一定要用括号 ===计算优先级高于||
+      this.switchOn();
+      sessionStorage.setItem('currentSwitch','on');
+      // else 中为另一种赋值写法
+    }
+    else {
+      this.switchOff();
+      sessionStorage['currentSwitch'] = 'off';
+    }
+
     this.getWindowSize();
     //this.getMousePos();
 
