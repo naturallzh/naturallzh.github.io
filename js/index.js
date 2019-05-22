6c0880a9044884dc71a4931d3257f441b60b5873
nuglues <406455645@qq.com>
2019-05-22 08:43:44 +0800

    update

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ let vm = new Vue({
   el: '#index-body',
   data: {
     show: true,
+    displayData: [],
   },
 
   beforeCreate: function () {
@@ -10,6 +11,7 @@ let vm = new Vue({
   },
   created: function () {
     // console.log('---created---');
+    this.setDisplayData();
   },
   beforeMount: function (){
     // console.log('---beforeMount---');
@@ -35,8 +37,21 @@ let vm = new Vue({
 
   computed: {},
   methods: {
-    changeShow: function () {
-      this.show = !this.show;
+    setDisplayData: function () {
+      this.displayData = [
+        {
+          title: "title1",
+          body: ["asdqwe","qweqwe","gggee"]
+        },
+        {
+          title: "title2",
+          body: ["5584","6722","361"]
+        },
+        {
+          title: "title3",
+          body: ["皇家华戒","杨弓","收割刀"]
+        }
+      ];
     }
   }
 });
