3d2f2c289db4fecb0bdf47ae4588e4992ca3fd19
nuglues <406455645@qq.com>
2019-05-23 18:05:15 +0800

    update

--- js/component.js
+++ js/component.js
@@ -157,3 +157,47 @@ Vue.component('blank-block', {
     setStyle("#blank-block",[0],"width:" + this.width + ";height:" + this.height + ";background:" + this.color);
   },
 });
+
+Vue.component('image-view', {
+  template:
+    '<div id="image-view-block">' +
+      '<div id="small-pic" @click="showFull()" :style="bgImgStyle"></div>' +
+      '<div class="mask56 bg" @click="closeFull()" v-show="showFullPic">' +
+        '<img id="full-pic" :src="url"/>' +
+        '<a id="download-btn" download="" @click.stop @touchmove.prevent :href="url">' +
+          '<div class="down-arrow"></div>' +
+        '</a>' +
+      '</div>' +
+    '</div>',
+
+  data: function () {
+    return {
+      showFullPic: false,  // 显示真实图片
+      bgImgStyle: "background-image:url('" + this.url + "')",
+    }
+  },
+  props: {
+    url: {
+      type: String,
+    },
+  },
+  computed: {
+  },
+  watch: {
+  },
+  methods: {
+    showFull: function () {
+      this.showFullPic = true;
+    },
+    closeFull: function () {
+      this.showFullPic = false;
+    },
+  },
+
+  beforeCreate () {},
+  created () {
+  },
+  beforeMount () {},
+  mounted (){
+  },
+});
---
+++ js/deco.js
@@ -0,0 +1,22 @@
+let vm = new Vue({
+  el: '#deco-body',
+  data: {
+    wideScr: document.body.offsetWidth > 1050,
+    menuData: [],
+  },
+  created: function () {
+    window.addEventListener('resize', this.checkWinWid);
+    this.menuData = [
+      {},
+      {},
+      {}
+    ];
+  },
+  computed: {
+  },
+  methods: {
+    checkWinWid: function () {
+      this.wideScr = document.body.offsetWidth > 1050;
+    },
+  }
+});
