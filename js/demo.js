20280e8356a4d6b71bd774c4c2542b1b1d3aa3b5
naturallzh <naturallzh@163.com>
2019-06-17 18:02:24 +0800

    update

--- js/demo.js
+++ js/demo.js
@@ -1,7 +1,43 @@
 let vm = new Vue({
   el: '#demo-body',
-  data: {},
-  created: function () {},
+  data: {
+    show: true,
+    displayData: [],
+  },
+  created: function () {
+    this.setDisplayData();
+  },
   computed: {},
-  methods: {}
+  methods: {
+
+    setDisplayData: function () {
+      this.displayData = [
+        {
+          title: "title1",
+          body: ["asdqwe","qweqwe","gggee","qaq"],
+          open: false,
+        },
+        {
+          title: "标题2",
+          body: ["5584","6722","361"],
+          open: true,
+        },
+        {
+          title: "暗黑3",
+          body: ["皇家华戒","杨弓","收割刀","收割刀","收割刀"],
+          open: true,
+        }
+      ];
+    },
+
+    switchState: function (idx) {
+      this.displayData[idx].open = !this.displayData[idx].open;
+    },
+
+    reportSize: function (idx) {
+      const hei = document.getElementsByClassName("content")[idx].offsetHeight;
+      console.log(hei);
+    }
+
+  }
 });
--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 let vm = new Vue({
   el: '#index-body',
   data: {
-    show: true,
-    displayData: [],
   },
 
   beforeCreate: function () {
@@ -11,7 +9,6 @@ let vm = new Vue({
   },
   created: function () {
     // console.log('---created---');
-    this.setDisplayData();
   },
   beforeMount: function (){
     // console.log('---beforeMount---');
@@ -37,35 +34,6 @@ let vm = new Vue({
 
   computed: {},
   methods: {
-
-    setDisplayData: function () {
-      this.displayData = [
-        {
-          title: "title1",
-          body: ["asdqwe","qweqwe","gggee","qaq"],
-          open: false,
-        },
-        {
-          title: "标题2",
-          body: ["5584","6722","361"],
-          open: true,
-        },
-        {
-          title: "暗黑3",
-          body: ["皇家华戒","杨弓","收割刀","收割刀","收割刀"],
-          open: true,
-        }
-      ];
-    },
-
-    switchState: function (idx) {
-      this.displayData[idx].open = !this.displayData[idx].open;
-    },
-
-    reportSize: function (idx) {
-      const hei = document.getElementsByClassName("content")[idx].offsetHeight;
-      console.log(hei);
-    }
 
   }
 });
