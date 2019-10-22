1dcc1fcf9db169dccf4b866f402412f90d087d7b
naturallzh <naturallzh@163.com>
2019-10-22 18:04:01 +0800

    update

--- js/drag_demo.js
+++ js/drag_demo.js
@@ -5,12 +5,24 @@ let vm = new Vue({
   },
   created () {},
   mounted () {
-    this.initData();
+    this.init();
   },
   computed: {},
   methods: {
-    initData () {
+    init () {
       this.listData = ['#1','#2','#3','#4','#5','#6','#7'];
+
+      const ele = document.getElementById('listBlock');
+      ele.addEventListener("mousedown", this.mousedownCheck, false);
+      // obj.addEventListener("mouseup", function () {console.log('up');});
+      // obj.addEventListener("mousemove", function () {console.log('move');});
+      // setTimeout(()=>{console.log(obj.offsetHeight);},0);
+    },
+    mousedownCheck () {
+      const ele = document.getElementById('listBlock');
+      // ele.addEventListener("mouseup", function () {console.log('up');});
+
+      console.log("long time mousedown detected");
     },
   }
 });
