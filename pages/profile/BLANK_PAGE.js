1b025f26e1aa13a2c80d873b2f9eab13cd07eb6d
naturallzh <46662172+naturallzh@users.noreply.github.com>
2020-02-06 23:38:50 +0800

    update

---
+++ pages/profile/BLANK_PAGE.js
@@ -0,0 +1,37 @@
+let vm = new Vue({
+  el: '#BLANK-PAGE-body',
+  data: {
+  },
+  beforeCreate () {
+    // console.log('---beforeCreate---');
+  },
+  created () {
+    // console.log('---created---');
+  },
+  beforeMount () {
+    // console.log('---beforeMount---');
+  },
+  mounted () {
+    // console.log('---mounted---');
+  },
+
+  /*
+  beforeUpdate () {
+    console.log('---beforeUpdate---');
+  },
+  updated () {
+    console.log('---updated---');
+  },
+  beforeDestory () {
+    console.log("---beforeDestory---");
+  },
+  destoryed () {
+    console.log("---destoryed---");
+  },
+  */
+
+  computed: {
+  },
+  methods: {
+  }
+});
