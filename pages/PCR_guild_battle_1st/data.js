2d6d4b16ee6e363c0b48b30c7d6b66818b45beaa
naturallzh <naturallzh@163.com>
2020-05-14 19:08:53 +0800

    update

--- pages/PCR_guild_battle_1st/data.js
+++ pages/PCR_guild_battle_1st/data.js
@@ -916,6 +916,7 @@ const DATA_actionData = [
 			{name: "爱丽丝", damage: 280432},
 			{name: "我群最菜", damage: 291466},
 			{name: "咸鱼", damage: 517670},
+			{name: "香浓朱古力", damage: 484729},
 		]
 	}
 ];
