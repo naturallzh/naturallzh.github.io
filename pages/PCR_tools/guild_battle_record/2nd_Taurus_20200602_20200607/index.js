bec1ce873a2863d4001eaee0cf13cf93c8d82b6c
naturallzh <naturallzh@163.com>
2020-06-05 23:23:15 +0800

    update

--- pages/PCR_tools/guild_battle_record/2nd_Taurus_20200602_20200607/index.js
+++ pages/PCR_tools/guild_battle_record/2nd_Taurus_20200602_20200607/index.js
@@ -8,7 +8,7 @@ let vm = new Vue({
     loadingMask: true,
 
     time: {
-      updateTime: new Date(2020,5,5,20,53),
+      updateTime: new Date(2020,5,5,22,41),
       startTime: new Date(2020,5,2,5),
       curTime: new Date(),
       endTime: new Date(2020,5,7,23,59,59),
