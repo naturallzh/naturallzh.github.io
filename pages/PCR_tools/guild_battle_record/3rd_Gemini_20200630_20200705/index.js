05da2b9462b0aca82941566e580cf22479313929
naturallzh <naturallzh@163.com>
2020-06-30 21:57:28 +0800

    update

--- pages/PCR_tools/guild_battle_record/3rd_Gemini_20200630_20200705/index.js
+++ pages/PCR_tools/guild_battle_record/3rd_Gemini_20200630_20200705/index.js
@@ -10,7 +10,7 @@ let vm = new Vue({
     loadingMask: true,
 
     time: {
-      updateTime: new Date(2020,5,30,21,9),
+      updateTime: new Date(2020,5,30,21,57),
       startTime: new Date(2020,5,30,5),
       curTime: new Date(),
       endTime: new Date(2020,6,5,23,59,59),
