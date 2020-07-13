d9365ffac5cb1059fb6784b9d2b16c8eedf4c1f7
naturallzh <naturallzh@163.com>
2020-07-13 23:23:45 +0800

    update

--- pages/schedule_system/index.js
+++ pages/schedule_system/index.js
@@ -10,29 +10,7 @@ let vm = new Vue({
   beforeMount () {
   },
   mounted () {
-    var oXL = new ActiveXObject("Excel.application");
-    const fileObj = oXL.Workbooks.open('data_list.xlsx');
-    impexcel(fileObj);
-    function impexcel(obj) {
-      console.log(obj)
-      if (!obj.files) {
-        return;
-      }
-      var f = obj.files[0];
-      //新建文件读取
-      var reader = new FileReader();
-      reader.onload = function(e) {
-        var data = e.target.result;
-        //将文件读取为二进制字符串
-        wb = XLSX.read(data, {type : 'binary'});
-        alert(wb.SheetNames[0]);
-        // wb.SheetNames[0]是获取Sheets中第一个Sheet的名字
-        // wb.Sheets[Sheet名]获取第一个Sheet的数据
-        JSON.stringify(XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]));
-        console.log(JSON.stringify(XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]])));
-      };
-      reader.readAsBinaryString(f);
-    }
+    this.init();
   },
 
   /*
@@ -44,5 +22,38 @@ let vm = new Vue({
   computed: {
   },
   methods: {
+
+    init: function () {
+
+      if (localStorage.getItem('dataArr')) {
+        let dataArr = localStorage.getItem('dataArr');
+        dataArr = JSON.parse(dataArr);
+        console.log(dataArr[0]);
+        //
+        // localStorage.removeItem("dataArr");
+      }
+      
+      const inputEle = document.getElementById("excel-file");
+      // 为input添加onchange事件
+      inputEle.onchange = function(e) {
+        const obj = e.target;
+        const f = obj.files[0];
+        const reader = new FileReader();
+        reader.onload = function(e) {
+          const data = e.target.result;
+          //将文件读取为二进制字符串
+          const wb = XLSX.read(data, {type : 'binary'});
+          // wb.SheetNames[0]是获取Sheets中第一个Sheet的名字
+          // wb.Sheets[Sheet名]获取第一个Sheet的数据
+          const dataArr = JSON.stringify(XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]));
+          // const dataArr = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]);
+
+          console.log(dataArr);
+          localStorage.setItem('dataArr', dataArr);
+        };
+        reader.readAsBinaryString(f);
+      };
+    },
+
   }
 });
