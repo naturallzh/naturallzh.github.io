dc1fd6dd28fcaec012480cb2236349d59f2a0b90
naturallzh <naturallzh@163.com>
2020-08-02 22:04:59 +0800

    update

--- pages/PCR_tools/guild_battle_record/4th_Cancer_20200728_20200802/index.js
+++ pages/PCR_tools/guild_battle_record/4th_Cancer_20200728_20200802/index.js
@@ -10,7 +10,7 @@ let vm = new Vue({
     loadingMask: true,
 
     time: {
-      updateTime: new Date(2020,7,2,21,51),
+      updateTime: new Date(2020,7,2,22,4),
       startTime: new Date(2020,6,28,5),
       curTime: new Date(),
       endTime: new Date(2020,7,2,23,59,59),
