716681181575bfb1f92496e4a9de0e52e56cbc93
naturallzh <naturallzh@163.com>
2020-08-04 17:17:13 +0800

    update

---
+++ index.js
@@ -0,0 +1,20 @@
+const vm = new Vue({
+  el: '#index-body',
+  data: {
+    isLoading: true,
+  },
+
+  computed: {
+  },
+
+  created () {},
+
+  mounted () {
+    this.isLoading = false;
+  },
+
+  destroyed () {},
+
+  methods: {
+  }
+});
